e84fadffc9a388544b7a79723243654bf770ba15
Jacson <jacson_ferraz98@hotmail.com>
2019-09-02 22:29:21 -0300

    Tela de login

---
+++ lib/login.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  var focusNomeUsuario = new FocusNode();
+  var focusSenhaUsuario = new FocusNode();
+
+  TextEditingController nomeUsuario = TextEditingController();
+  TextEditingController senhaUsuario = TextEditingController();
+
+  GlobalKey<FormState> formkey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Login"),
+          centerTitle: true,
+        ),
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          padding: EdgeInsets.all(50.0),
+          child: Form(
+            key: formkey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextFormField(
+                  focusNode: focusNomeUsuario,
+                  autofocus: true,
+                  textInputAction: TextInputAction.next,
+                  validator
+                  : (valor) {
+                    if (valor.isEmpty) {
+                      FocusScope.of(context).requestFocus(focusNomeUsuario);
+                      return "Informe o nome";
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                      labelText: "Usuário", icon: Icon(Icons.people)),
+                  keyboardType: TextInputType.text,
+                  controller: nomeUsuario,
+                ),
+               
+                TextFormField(
+                  focusNode: focusSenhaUsuario,
+                  autofocus: true,
+                  obscureText: true,
+                  textInputAction: TextInputAction.next,
+                  validator: (valor) {
+                    if (valor.isEmpty) {
+                      FocusScope.of(context).requestFocus(focusSenhaUsuario);
+                      return "Informe a senha";
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                      labelText: "Senha", icon: Icon(Icons.lock)),
+                  keyboardType: TextInputType.text,
+                  controller: senhaUsuario,
+                ),
+               SizedBox(height: 50,),
+            RaisedButton(
+                 child: Text(
+                    "Login",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  color: Colors.blue,
+                  onPressed: () {
+                     if (formkey.currentState.validate()) {
+                      setState(() {
+                      });
+            }
+                      }
+            ),
+              ],
+            ),
+          ),
+        ));
+  }
+  }
+                  
+
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:interdisciplinar/login.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Login(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
